db4bd3f5480df52ad848d5edb3fb7b9ba54c6f93
Menkz <iammenkz@gmail.com>
2019-04-07 20:26:44 +1000

    Update ReadMe & resource.lua

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # esx_poolcleaner
+
+Modified Pool Cleaner Job script for ESX. Originally by https://github.com/Cha0sNighT
+
+# Getting Started
+
+1. Add esx_poolcleaner folder into resources
+2. Add in server.cfg
+3. Add esx_poolcleaner.sql to database
+
+# Configuration
+
+1. Configuration only in config.lua
+
+Thanks to Anthony for esx_garbage, wich is the base of all of this.
+https://github.com/AnthonyEl
--- __resource.lua
+++ __resource.lua
@@ -5,15 +5,17 @@ description 'ESX Pool Cleaner'
 version '1.0'
 
 client_scripts {
-  '@es_extended/locale.lua',
-  'locales/en.lua',
-  'client/main.lua',
-  'config.lua'	
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'client/main.lua',
+	'config.lua'	
 }
 
 server_scripts {
-  '@es_extended/locale.lua',
-  'locales/en.lua',
-  'server/main.lua',
-  'config.lua'
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'server/main.lua',
+	'config.lua'
 }
