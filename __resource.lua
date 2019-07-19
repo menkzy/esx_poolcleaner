8a8ef7cb1f349893609f92c745d5b23c72cef394
Menkz <iammenkz@gmail.com>
2019-07-20 04:30:25 +1000

    Fixed the handling of the bison
    Vehicle sometimes tipped over at high speeds.

--- __resource.lua
+++ __resource.lua
@@ -13,10 +13,8 @@ client_scripts {
 	'vehicles.meta',
 	'carvariations.meta',
 	'carcols.meta',
-	'handling.meta',
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
